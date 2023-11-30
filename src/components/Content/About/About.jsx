a7d96b2c18308b3abfc6adb3807986fa107dc782
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2023-11-30 11:52:53 +0100

    Project wording edits

--- src/components/Content/About/About.jsx
+++ src/components/Content/About/About.jsx
@@ -1,11 +1,11 @@
 function About() {
   return (
     <section data-section id="about" className='about-container sunHover'>
-        <p><span className="bold">Hi <i id="hand" className="fa-regular fa-hand"></i> My name is Patrick</span> and I´m an Irish front end engineer (and aspiring full-stack) based in sunny Spain.</p>
+        <p><span className="bold">Hi <i id="hand" className="fa-regular fa-hand"></i> My name is Patrick</span> and I´m an Irish front end (and aspiring full-stack) engineer, based in sunny Spain.</p>
 
-        <p>For the past 6+ years I´ve run a <a href="https://patrickalvarezwebdesign.com/">freelance web design company</a> whilst also <span className="bold">managing teams of up to a 150 people</span> in an educational company. A few months ago I finally chose to take the plunge and shift into focusing 100% on my real passion - <span className="bold">coding awesome stuff!</span></p>
+        <p>For the past 6+ years I´ve run a <a href="https://patrickalvarezwebdesign.com/">freelance web design company</a> whilst also <span className="bold">managing teams of up to a 150 people</span> in an educational company. A few months ago, I took the plunge and made the shift into focusing 100% on my real passion - <span className="bold">coding awesome stuff!</span></p>
 
-        <p>When I´m not at the computer, I´m usually hanging out with my wife and pets, playing boardgames with friends, or enjoying <a id="sun">
+        <p>When I´m not at the computer, I´m usually hanging out with my wife and pets, playing board games with friends, or enjoying <a id="sun">
           <span className="rouge">t</span>
           <span className="orange">i</span>
           <span className="jaune">m</span>
@@ -18,9 +18,7 @@ function About() {
           <span className="jaune">s</span>
           <span className="vert">u</span>
           <span className="bleu">n.</span>
-        </a></p>
-
-        
+        </a></p>      
 
   </section>
   )
