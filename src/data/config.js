995621de077601d493dfd506c70e77bef9f4bb21
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2024-09-28 14:49:57 +0200

    MOD: Change CV url

--- src/data/config.js
+++ src/data/config.js
@@ -2,5 +2,5 @@ export default {
     GITHUB_URL: 'https://github.com/Patchalv',
     LINKEDIN_URL: 'https://www.linkedin.com/in/patrickalvarezeades',
     EMAIL_URL: 'mailto:p.alvarezeades@gmail.com',
-    CV_URL: 'https://drive.google.com/file/d/1GeYTG9JXn_J5ZDPf3ymsb4r8ouPpyHEO/view?usp=sharing',
+    CV_URL: 'https://drive.google.com/file/d/1HCuV5WZ0hPMe3DN_c1DeCkHNmTSl621x/view?usp=sharing',
 };
